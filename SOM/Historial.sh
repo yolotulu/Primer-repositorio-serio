4ad8c4f131a3e0c9458ccb527129f1c2ade360e4
yolotp <ericogoyo@gmail.com>
2017-06-22 11:14:39 +0200

    Update Historial.sh

--- SOM/Historial.sh
+++ SOM/Historial.sh
@@ -5,5 +5,7 @@
 # "history" es el historial de todo lo que hicimos en el terminal.
 # Lo que hace ">" es redireccionar algo (history, un echo...)
 # al lugar deseado.
+:: "|" (pipe) es un comando hace un comando dentro de otro sin que necesite aparecer otra ventana.
+:: "more" mira las primeras lineas de un documento.
 
-history > historial.txt
+history > historial.txt | more
