4eac2237102055d9818778b1f3cb3ba00a0a814a
yolotp <ericogoyo@gmail.com>
2017-06-22 11:13:08 +0200

    Update Historial.cmd

--- SOM/Historial.cmd
+++ SOM/Historial.cmd
@@ -5,5 +5,7 @@
 :: "doskey /history" es el historial de todo lo que hicimos en el terminal.
 :: Lo que hace ">" es redireccionar algo (doskey /history, un echo...)
 :: al lugar deseado.
+:: "|" (pipe) es un comando hace un comando dentro de otro sin que necesite aparecer otra ventana.
+:: "more" mira las primeras lineas de un documento.
 
-doskey /history > historial.txt
+doskey /history > historial.txt | more
